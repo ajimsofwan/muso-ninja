02888e69b389e4176148c8cb22e57741ac2f8297
Aji Muhammad Sofwan <ajimsofwan@gmail.com>
2023-11-15 17:23:04 +0700

    feat: configure firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "musoninjas-ca821.firebaseapp.com",
+  projectId: "musoninjas-ca821",
+  storageBucket: "musoninjas-ca821.appspot.com",
+  messagingSenderId: "342672158163",
+  appId: "1:342672158163:web:6c13d8f0baad1abdded326",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { auth, firestore, timestamp };
